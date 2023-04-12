4fb1aa7eee371b906dd7f8bcee3176f13040bc6f
vinay sharma <vinay.sharma@crowdbotics.com>
2023-04-12 14:50:52 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/NewScreenCopy/index.js
@@ -0,0 +1,41 @@
+import { TextInput } from "react-native";
+import { Pressable } from "react-native";
+import React from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled4 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <Pressable><ScrollView contentContainerStyle={{
+        backgroundColor: '#f0f0f1',
+        padding: 10,
+        position: 'relative',
+        flex: 1
+      }}><View style={styles.ofEGjHtU}></View><View style={styles.BjMpdrkz}></View></ScrollView></Pressable>
+    <TextInput style={styles.CWYDAYoq} placeholder="test sadasde"></TextInput></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  ofEGjHtU: {
+    width: 80,
+    height: 80,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 40
+  },
+  BjMpdrkz: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  CWYDAYoq: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 180,
+    height: 150
+  }
+});
+export default Untitled4;
